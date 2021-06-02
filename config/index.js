a9dd2065c50e3fefe9ce277c13209af44804f0ff
Mickey <daiglewilliam13@gmail.com>
2021-06-02 12:00:00 -0700

    debug image upload, hide API Key

--- config/index.js
+++ config/index.js
@@ -1,6 +1,18 @@
 const Cloud = require('@google-cloud/storage')
 const path = require('path')
-const serviceKey = path.join(__dirname, './flavor-theory-26ffb238e4da.json')
+const serviceKey = {
+  type: process.ENV.type,
+  project_id: process.ENV.project_id,
+  private_key_id: process.ENV.private_key_id,
+  private_key: process.ENV.private_key,
+  client_email: process.ENV.client_email,
+  client_id: process.ENV.client_id,
+  auth_uri: process.ENV.auth_uri,
+  token_uri: process.ENV.token_uri,
+  auth_provider_x509_cert_url: process.ENV.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.ENV.auth_provider_x509_cert_url,
+}
+
 
 const { Storage } = Cloud
 const storage = new Storage({

ab3ce425edb28fb9e098dcebdb4022f0c67874f4
Mickey <daiglewilliam13@gmail.com>
2021-06-02 12:09:47 -0700

    debug config vars

--- config/index.js
+++ config/index.js
@@ -1,16 +1,16 @@
 const Cloud = require('@google-cloud/storage')
 const path = require('path')
 const serviceKey = {
-  type: process.ENV.TYPE,
-  project_id: process.ENV.PROJECT_ID,
-  private_key_id: process.ENV.PRIVATE_KEY_ID,
-  private_key: process.ENV.PRIVATE_KEY,
-  client_email: process.ENV.CLIENT_EMAIL,
-  client_id: process.ENV.CLIENT_ID,
-  auth_uri: process.ENV.AUTH_URI,
-  token_uri: process.ENV.TOKEN_URI,
-  auth_provider_x509_cert_url: process.ENV.AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.ENV.CLIENT_X509_CERT_URL,
+  "type": process.ENV.TYPE,
+  "project_id": process.ENV.PROJECT_ID,
+  "private_key_id": process.ENV.PRIVATE_KEY_ID,
+  "private_key": process.ENV.PRIVATE_KEY,
+  "client_email": process.ENV.CLIENT_EMAIL,
+  "client_id": process.ENV.CLIENT_ID,
+  "auth_uri": process.ENV.AUTH_URI,
+  "token_uri": process.ENV.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.ENV.AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.ENV.CLIENT_X509_CERT_URL,
 }
 
 
